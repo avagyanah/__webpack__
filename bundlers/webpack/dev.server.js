8ba3f772795a7ebc95f19469eb6c6aa3d8d75ffa
Ashot Avagyan <avagyanah@gmail.com>
2020-07-23 19:28:41 +0400

    webpack-dev-server folder

---
+++ bundlers/webpack/dev.server.js
@@ -0,0 +1,16 @@
+const webpack = require('webpack');
+const WebpackDevServer = require('webpack-dev-server');
+const devConfig = require('./webpack.dev')();
+const port = require('minimist')(process.argv.slice(2)).port || 3000;
+
+try {
+    const server = new WebpackDevServer(webpack(devConfig), {
+        port,
+        watchOptions: { aggregateTimeout: 0 },
+        stats: { all: false, colors: true },
+        clientLogLevel: 'error',
+    });
+    server.listen(port, '0.0.0.0');
+} catch (err) {
+    console.error(err);
+}
